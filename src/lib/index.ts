7d4f1b72f42a8ae971594771967310e060f68acb
Sam-Izdat <ghsamizdat@gmail.com>
2024-10-30 11:43:50 -0500

    Initial commit

---
+++ src/lib/index.ts
@@ -0,0 +1,16 @@
+export * from './canvas_tools';
+export * from './color';
+export * from './doc_types';
+export * from './globals';
+export * from './keybind';
+export * from './keymap';
+export * from './log';
+export * from './modals';
+export * from './panes';
+export * from './strings';
+
+// dir
+export * from './components';
+export * from './handlers';
+
+// stores, sotage, styles and templates should not be exported from $lib
