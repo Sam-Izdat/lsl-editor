eb4961496ee6c74ba8a0a4debbf4c12e5122228c
Sam-Izdat <ghsamizdat@gmail.com>
2024-11-18 12:27:40 -0600

    offline service worker

--- src/routes/(canvasframe)/canvasframe/+layout.svelte
+++ src/routes/(canvasframe)/canvasframe/+layout.svelte
@@ -1,17 +1,1 @@
-<script>
-  import { browser } from '$app/environment';
-  if (browser) {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js', {
-          scope: '.' 
-      }).then(function(registration) {
-          // Registration was successful
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-          // registration failed :(
-          console.log('ServiceWorker registration failed: ', err);
-      });
-    }
-  }
-</script>
 <slot />
