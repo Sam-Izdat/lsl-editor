fb46943f277222a6ef9314655e17de1d58e194eb
Sam-Izdat <ghsamizdat@gmail.com>
2024-11-19 02:33:53 -0600

    service worker

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -39,15 +39,15 @@
   });
 
   if (browser && 'serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js', {
-        scope: '.' 
-    }).then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-    });
+    // navigator.serviceWorker.register('/service-worker.js', {
+    //     scope: '.' 
+    // }).then(function(registration) {
+    //     // Registration was successful
+    //     console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    // }, function(err) {
+    //     // registration failed :(
+    //     console.log('ServiceWorker registration failed: ', err);
+    // });
     self.addEventListener('fetch', (event) => {
       event.respondWith(
         (async () => {
