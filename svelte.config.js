7bb0f4e19d1a97f524b863e2762f6afa9aca8137
Sam-Izdat <ghsamizdat@gmail.com>
2024-11-19 16:44:44 -0600

    service worker

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
   	prerender: {
 			crawl: true,
 			entries: process.env.BUILD_TYPE === 'static' 
-				? ['/', '/canvasframe', '/open-file', '/get-gist', '/get-url', '/gist/0', '/url/0'] // static routes here
+				? ['/', '/open-file', '/get-gist', '/get-url', '/gist/0', '/url/0'] // static routes here
 				: [] // leave empty dynamic routes
 		},
     paths: {
