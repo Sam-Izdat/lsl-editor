714c156a6b6f18c6eb6fe137084d101ab296e759
Sam-Izdat <ghsamizdat@gmail.com>
2024-11-20 18:09:37 -0600

    fix prot url decoding

--- src/routes/(simple)/pth/+page.svelte
+++ src/routes/(simple)/pth/+page.svelte
@@ -10,16 +10,18 @@
     const url = new URL(window.location.href);  
     const searchParams = new URLSearchParams(url.search); 
 
-    appURL = searchParams.has('q') ? new URL(decodeURIComponent(searchParams.get('q'))) : null;
+    const appHref = searchParams.get('q');
+    appURL = searchParams.has('q') ? new URL(appHref) : null;
 
     if (appURL) {
       if (appURL.hostname == 'url' && appURL.pathname) {
-        outURL = './get-url?q='+encodeURIComponent(appURL.pathname.replace(/^\/+/, '')) ?? '';
+        let remoteURL = new URL(appHref).pathname.replace(/^\/+/, '');
+        outURL = './get-url?q='+remoteURL ?? '';
       } else if (appURL.hostname == 'gist' && appURL.pathname) {
         outURL = './get-gist?q='+appURL.pathname.split('/')[1] ?? '';
       } else if (appURL.hostname) {
         // assume actual URL
-        let remoteURL = guessRawURL(appURL.href.replace('web+'+cfg.PWA_URL_PATTERN, 'https'));
+        let remoteURL = guessRawURL(appHref.replace('web+'+cfg.PWA_URL_PATTERN, 'https'));
         outURL = './get-url?q='+encodeURIComponent(remoteURL) ?? '';
       }
       appURL.searchParams.forEach((value, key) => {
